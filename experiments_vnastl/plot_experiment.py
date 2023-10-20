1764313fad410461a33af4ee0935f50021a126f2
Vivian Nastl <vivian.nastl@tuebingen.mpg.de>
2023-10-20 12:12:06 +0200

    update plots

--- experiments_vnastl/plot_experiment.py
+++ experiments_vnastl/plot_experiment.py
@@ -16,14 +16,18 @@ from  statsmodels.stats.proportion import proportion_confint
 plt.rcParams['figure.dpi'] = 300
 plt.rcParams['savefig.dpi'] = 300
 
+import os
+os.chdir("/Users/vnastl/Seafile/My Library/mpi project causal vs noncausal/tableshift")
 #%%
 if __name__ == '__main__':
-    experiment_name = "college_scorecard"
-    experiments=["college_scorecard","college_scorecard_causal","college_scorecard_causal_no_tuition_fee"]
+    # experiment_name = "college_scorecard"
+    # experiments=["college_scorecard","college_scorecard_causal","college_scorecard_causal_no_tuition_fee"]
     # experiment_name = "acsunemployment"
     # experiments = ["acsunemployment","acsunemployment_causal", "acsunemployment_anticausal"]
+    experiment_name = "acspubcov"
+    experiments = ["acspubcov","acspubcov_causal"]
     # experiment_name = "physionet"
-    # experiments = ["physionet","physionet_causal", "physionet_anticausal"] 
+    # experiments = ["physionet"] #,"physionet_causal", "physionet_anticausal"] 
     cache_dir="tmp"
     
     eval_all = pd.DataFrame()
@@ -92,7 +96,7 @@ if __name__ == '__main__':
             'ood_test_lb':eval_constant['ood_test_conf'][0],
             'ood_test_ub':eval_constant['ood_test_conf'][1],
             'features':'constant',
-            'model':run.split("_")[0]}])
+            'model':'constant'}])
     eval_all = pd.concat([eval_all, eval_pd], ignore_index=True)
 
         
@@ -114,6 +118,7 @@ if __name__ == '__main__':
             yerr=eval_plot['ood_test_ub']-eval_plot['ood_test'], fmt="o",
             color=colormap(i), ecolor=colormap(i), label=str(features))
     plt.legend(title="Feature")
+
     eval_plot = eval_all[eval_all['features']=="constant"]
     plt.fill_between([0, min(eval_plot['id_test'])],0,1,
                         color="tab:grey")
@@ -121,55 +126,56 @@ if __name__ == '__main__':
                         color="tab:grey")
     # Plot the diagonal line
     plt.plot([0, 1], [0, 1], color='black', linestyle='dashed')
-    plt.xlim((eval_constant['id_test']-0.001, max(eval_all['id_test'])+0.001))
-    plt.ylim((eval_constant['ood_test']-0.001,max(eval_all['ood_test'])+0.001))
+    plt.xlim((eval_constant['id_test']-0.01, max(eval_all['id_test'])+0.01))
+    plt.ylim((eval_constant['ood_test']-0.01,max(eval_all['ood_test'])+0.01))
     plt.savefig(str(Path(__file__).parents[0]/f"plot_{experiment_name}_causal_vs_all"))
     plt.show()
 
 
     # %%
-    plt.title(
-        f"Tableshift: {experiment_name}")
-    plt.xlabel(f"id accuracy)")
-    plt.ylabel(f"ood accuracy")
-    colors = pd.factorize(eval_all["model"])[0]
-    colormap = cm.get_cmap('tab20')
-    eval_causal = eval_all[eval_all['features']=="causal"]
-    unique_models = eval_all['model'].unique()
-    for j, model in enumerate(unique_models):
-        eval_plot = eval_causal[eval_causal['model']==model]
+    if "causal" in feature_selection:
+        plt.title(
+            f"Tableshift: {experiment_name}")
+        plt.xlabel(f"id accuracy)")
+        plt.ylabel(f"ood accuracy")
+        colors = pd.factorize(eval_all["model"])[0]
+        colormap = cm.get_cmap('tab20')
+        eval_causal = eval_all[eval_all['features']=="causal"]
+        unique_models = eval_all['model'].unique()
+        for j, model in enumerate(unique_models):
+            eval_plot = eval_causal[eval_causal['model']==model]
+            errors = plt.errorbar(
+                x=eval_plot['id_test'],
+                y=eval_plot['ood_test'],
+                xerr=eval_plot['id_test_ub']-eval_plot['id_test'],
+                yerr=eval_plot['ood_test_ub']-eval_plot['ood_test'], fmt="o",
+                color=colormap(j), ecolor=colormap(j), label=str(model))
+        
+        eval_plot = eval_all[eval_all['features']=="all"]
         errors = plt.errorbar(
-            x=eval_plot['id_test'],
-            y=eval_plot['ood_test'],
-            xerr=eval_plot['id_test_ub']-eval_plot['id_test'],
-            yerr=eval_plot['ood_test_ub']-eval_plot['ood_test'], fmt="o",
-            color=colormap(j), ecolor=colormap(j), label=str(model))
-    
-    eval_plot = eval_all[eval_all['features']=="all"]
-    errors = plt.errorbar(
-            x=eval_plot['id_test'],
-            y=eval_plot['ood_test'],
-            xerr=eval_plot['id_test_ub']-eval_plot['id_test'],
-            yerr=eval_plot['ood_test_ub']-eval_plot['ood_test'], fmt="o",
-            color=colormap(19), ecolor=colormap(19), label=str(model), alpha=0.3)
-    plt.fill_between([0, max(eval_plot['id_test'])],
-                        [0,0],
-                        [max(eval_plot['ood_test']),max(eval_plot['ood_test'])],
-                        color=colormap(20), alpha=0.3)
-    
-    eval_plot = eval_all[eval_all['features']=="constant"]
-    plt.fill_between([0, min(eval_plot['id_test'])],0,1,
-                        color="tab:grey")
-    plt.fill_between([0, 1],0,max(eval_plot['ood_test']),
-                        color="tab:grey")
+                x=eval_plot['id_test'],
+                y=eval_plot['ood_test'],
+                xerr=eval_plot['id_test_ub']-eval_plot['id_test'],
+                yerr=eval_plot['ood_test_ub']-eval_plot['ood_test'], fmt="o",
+                color=colormap(19), ecolor=colormap(19), label=str(model), alpha=0.3)
+        plt.fill_between([0, max(eval_plot['id_test'])],
+                            [0,0],
+                            [max(eval_plot['ood_test']),max(eval_plot['ood_test'])],
+                            color=colormap(20), alpha=0.3)
+        
+        eval_plot = eval_all[eval_all['features']=="constant"]
+        plt.fill_between([0, min(eval_plot['id_test'])],0,1,
+                            color="tab:grey")
+        plt.fill_between([0, 1],0,max(eval_plot['ood_test']),
+                            color="tab:grey")
 
-    plt.legend(title="Model (causal)")
-    # Plot the diagonal line
-    plt.plot([0, 1], [0, 1], color='black', linestyle='dashed')
-    plt.xlim((eval_constant['id_test']-0.001, max(eval_all['id_test'])+0.001))
-    plt.ylim((eval_constant['ood_test']-0.001,max(eval_all['ood_test'])+0.001))
-    plt.savefig(str(Path(__file__).parents[0]/f"plot_{experiment_name}_causal_models"))
-    plt.show()
+        plt.legend(title="Model (causal)")
+        # Plot the diagonal line
+        plt.plot([0, 1], [0, 1], color='black', linestyle='dashed')
+        plt.xlim((eval_constant['id_test']-0.01, max(eval_all['id_test'])+0.01))
+        plt.ylim((eval_constant['ood_test']-0.01,max(eval_all['ood_test'])+0.01))
+        plt.savefig(str(Path(__file__).parents[0]/f"plot_{experiment_name}_causal_models"))
+        plt.show()
     
     # %%
     if "causal without tuition" in feature_selection:
@@ -211,8 +217,8 @@ if __name__ == '__main__':
         plt.legend(title="Model (causal with no tuition)")
         # Plot the diagonal line
         plt.plot([0, 1], [0, 1], color='black', linestyle='dashed')
-        plt.xlim((eval_constant['id_test']-0.001, max(eval_all['id_test'])+0.001))
-        plt.ylim((eval_constant['ood_test']-0.001,max(eval_all['ood_test'])+0.001))
+        plt.xlim((eval_constant['id_test']-0.01, max(eval_all['id_test'])+0.01))
+        plt.ylim((eval_constant['ood_test']-0.01,max(eval_all['ood_test'])+0.01))
         plt.savefig(str(Path(__file__).parents[0]/f"plot_{experiment_name}_causal_models_without_tuition"))
         plt.show()
     
@@ -255,8 +261,8 @@ if __name__ == '__main__':
         plt.legend(title="Model (anticausal)")
         # Plot the diagonal line
         plt.plot([0, 1], [0, 1], color='black', linestyle='dashed')
-        plt.xlim((eval_constant['id_test']-0.001, max(eval_all['id_test'])+0.001))
-        plt.ylim((eval_constant['ood_test']-0.001,max(eval_all['ood_test'])+0.001))
+        plt.xlim((eval_constant['id_test']-0.01, max(eval_all['id_test'])+0.01))
+        plt.ylim((eval_constant['ood_test']-0.01,max(eval_all['ood_test'])+0.01))
         plt.savefig(str(Path(__file__).parents[0]/f"plot_{experiment_name}_anticausal_models"))
         plt.show()
 
@@ -275,12 +281,17 @@ if __name__ == '__main__':
             y=eval_plot['ood_test'],
             xerr=eval_plot['id_test_ub']-eval_plot['id_test'],
             yerr=eval_plot['ood_test_ub']-eval_plot['ood_test'], fmt="o",
-            color=colormap(j), ecolor=colormap(j), label=str(model))
+            color=colormap(j), ecolor=colormap(j), label=str(model), alpha=0.5)
     plt.legend(title="Model")
     # Plot the diagonal line
     plt.plot([0, 1], [0, 1], color='black', linestyle='dashed')
-    plt.xlim((eval_constant['id_test']-0.001, max(eval_all['id_test'])+0.001))
-    plt.ylim((eval_constant['ood_test']-0.001,max(eval_all['ood_test'])+0.001))
+    eval_plot = eval_all[eval_all['features']=="constant"]
+    plt.fill_between([0, min(eval_plot['id_test'])],0,1,
+                        color="tab:grey")
+    plt.fill_between([0, 1],0,max(eval_plot['ood_test']),
+                        color="tab:grey")
+    plt.xlim((eval_constant['id_test']-0.01, max(eval_all['id_test'])+0.01))
+    plt.ylim((eval_constant['ood_test']-0.01,max(eval_all['ood_test'])+0.01))
     plt.savefig(str(Path(__file__).parents[0]/f"plot_{experiment_name}_models"))
     plt.show()
     
