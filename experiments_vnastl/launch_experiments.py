e166a94073ed23c062a41f82d983d2e27a53857a
Vivian Nastl <vivian.nastl@tuebingen.mpg.de>
2023-12-22 15:04:44 +0100

    cluster: more memory

--- experiments_vnastl/launch_experiments.py
+++ experiments_vnastl/launch_experiments.py
@@ -23,7 +23,7 @@ N_TRIALS    = 1
 # Cluster settings
 JOB_MIN_BID = 50    # htcondor bid (min. is 15 apparently...)
 JOB_CPUS = 1     # number of CPUs per experiment (per cluster job)
-JOB_MEMORY_GB = 64    # GBs of memory
+JOB_MEMORY_GB = 128   # GBs of memory
 BIG_JOB_MEMORY_GB = 256
 
 VERBOSE = True
