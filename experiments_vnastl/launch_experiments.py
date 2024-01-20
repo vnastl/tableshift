d106ea09c45550479a0af36c0a768f7d42aa8420
Vivian Nastl <vivian.nastl@tuebingen.mpg.de>
2024-01-20 16:29:22 +0100

    cluster: relaunch college scorecard

--- experiments_vnastl/launch_experiments.py
+++ experiments_vnastl/launch_experiments.py
@@ -42,7 +42,7 @@ N_TRIALS    = 50
 # Cluster settings
 JOB_MIN_BID = 100    # htcondor bid (min. is 15 apparently...)
 JOB_CPUS = 1     # number of CPUs per experiment (per cluster job)
-JOB_MEMORY_GB = 256  # GBs of memory
+JOB_MEMORY_GB = 128  # GBs of memory
 BIG_JOB_MEMORY_GB = 256
 
 VERBOSE = True
@@ -53,9 +53,9 @@ TASKS = [
     # "acsincome_arguablycausal",
     # "acsincome_anticausal",
 
-    "acspubcov",
-    "acspubcov_causal",
-    "acspubcov_arguablycausal",
+    # "acspubcov",
+    # "acspubcov_causal",
+    # "acspubcov_arguablycausal",
 
     # "acsfoodstamps",
     # "acsfoodstamps_causal",
@@ -84,7 +84,7 @@ TASKS = [
     # "brfss_blood_pressure_arguablycausal",
     # "brfss_blood_pressure_anticausal",
 
-    # "college_scorecard",
+    "college_scorecard",
     # "college_scorecard_causal",
     # "college_scorecard_arguablycausal",
 
